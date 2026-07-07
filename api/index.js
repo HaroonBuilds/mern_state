f0638da07b36181694c0d4572dc5f3b6b0d7ed24
HaroonBuilds <askdeveloper009@gmail.com>
2026-07-07 17:19:21 +0500

    connect to datbase

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
 import express from 'express'
-
+import mongoose from "mongoose"
+import dotenv from 'dotenv'
+dotenv.config()
 const app = express();
+
+mongoose.connect(process.env.DB_URL).then(()=>{
+        console.log("database is connected successfully")
+}).catch(console.log("error while connecting to the database"))
 
 app.listen(4000,()=>{
     console.log(`server is runing on port 400`);
