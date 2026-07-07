223f8f52fd780485f3d372f4f77801430bba10ac
HaroonBuilds <askdeveloper009@gmail.com>
2026-07-07 13:55:39 +0500

    all pages and router are created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Singup from "./pages/Singup";
+import Singin from "./pages/Singin";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
 
 function App() {
   return (
-    <div>
-      <h1 className='bg-red-200'>realestate project</h1>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}>HOME</Route>
+        <Route path="signup" element={<Singup/>}>SIGNUP</Route>
+        <Route path="signin" element={<Singin/>}>SIGNIN</Route>
+        <Route path="profile" element={<Profile/>}>PROFILE</Route>
+        <Route path="about" element={<About/>}>ABOUT</Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
