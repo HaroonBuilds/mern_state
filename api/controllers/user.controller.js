3e7718c22de8680ec21a69d9bab667fc7270a472
HaroonBuilds <askdeveloper009@gmail.com>
2026-07-07 20:52:58 +0500

    create and test a test api

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,5 @@
+const test = function(req,res){
+    res.json({nessage:"testing is runing"})
+}
+
+export default test
--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
-import express from 'express'
-import mongoose from "mongoose"
-import dotenv from 'dotenv'
+import express from 'express';
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import userRouter from './router/user.router.js'
 dotenv.config()
 const app = express();
+app.use("/api/user",userRouter)
 
 mongoose.connect(process.env.DB_URL).then(()=>{
         console.log("database is connected successfully")
